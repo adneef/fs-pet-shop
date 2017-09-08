a824b1d0e3c63d120d682b06e0fb24d991225a97
Adam Neef <adneef@gmail.com>
2017-09-08 08:09:08 -0600

    httpServer, added POST functionality

--- httpServer.js
+++ httpServer.js
@@ -6,8 +6,8 @@ let http = require('http')
 let port = process.env.PORT || 8000
 
 let server = http.createServer(function(req, res) {
-  if(req.method === 'GET' && req.url === '/pets') {
-    fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+  if (req.method === 'GET' && req.url === '/pets') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
       if (err) {
         console.error(err)
         res.statusCode = 500
@@ -17,10 +17,9 @@ let server = http.createServer(function(req, res) {
       res.setHeader('Content-Type', 'application/json')
       res.end(petsJSON)
     })
-  }
-  else if (req.method === 'GET' && req.url === '/pets/0') {
+  } else if (req.method === 'GET' && req.url === '/pets/0') {
     fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
-      if(err){
+      if (err) {
         console.error(err)
         res.statusCode = 500
         res.setHeader('Content-Type', 'text/plain')
@@ -33,28 +32,84 @@ let server = http.createServer(function(req, res) {
       res.setHeader('Content-Type', 'application/json')
       res.end(petJSON)
     })
+  } else if (req.method === 'GET' && req.url === '/pets/1') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err)
+        res.statusCode = 500
+        res.setHeader('Content-Type', 'text/plain')
+        return res.end('Internal Server Error')
+      }
+
+      let pets = JSON.parse(petsJSON)
+      let petJSON = JSON.stringify(pets[1])
+
+      res.setHeader('Content-Type', 'application/json')
+      res.end(petJSON)
+    })
+  } else if (req.method === 'POST' && req.url === '/pets') {
+
+    let bodyJSON = ''
+
+    req.on('data', function(chunk) {
+      bodyJSON += chunk.toString()
+    })
+
+    req.on('end', function() {
+      fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+        if (err) {
+          console.error(err.stack);
+          res.statusCode = 500
+          res.setHeader('Content-Type', 'text/plain')
+          return res.end('Internal Server Error')
+        }
+
+        let pets = JSON.parse(petsJSON)
+        let body = JSON.parse(bodyJSON)
+
+        let age = Number.parseInt(body.age)
+        let kind = body.kind
+        let name = body.name
+
+        if (Number.isNaN(age) || !kind || !name) {
+          res.statusCode = 400
+          res.setHeader('Content-Type', 'text/plain')
+          res.end('Bad Request')
+
+          return
+        }
+
+        let pet = {
+          age,
+          kind,
+          name
+        }
+        pets.push(pet)
+
+        let updatedPetsJSON = JSON.stringify(pets)
+        let petJSON = JSON.stringify(pet)
+
+        fs.writeFile(petsPath, updatedPetsJSON, function(err) {
+          if (err) {
+            console.error(err.stack);
+
+            res.statusCode = 500
+            res.setHeader('Content-Type', 'text/plain')
+            res.end('Internal Server Error')
+
+            return
+          }
+
+          res.setHeader('Content-Type', 'application/json')
+          res.end(petJSON)
+        })
+      })
+    })
+  } else {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Not Found')
   }
-else if (req.method === 'GET' && req.url === '/pets/1') {
-  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
-    if(err){
-      console.error(err)
-      res.statusCode = 500
-      res.setHeader('Content-Type', 'text/plain')
-      return res.end('Internal Server Error')
-    }
-
-    let pets = JSON.parse(petsJSON)
-    let petJSON = JSON.stringify(pets[1])
-
-    res.setHeader('Content-Type', 'application/json')
-    res.end(petJSON)
-  })
-}
-else {
-  res.statusCode = 404
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Not Found')
-}
 })
 
 server.listen(port, function() {
