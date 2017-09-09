2af402a2bfc242b58bdb0b1f70cb2fa70adec961
Adam Neef <adneef@gmail.com>
2017-09-09 15:07:42 -0600

    restful server, passes all tests for CRUD

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.get('/pets', function(req, res) {
   fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
     if(readErr) {
-      console.log(readErr.stack)
+      console.error(readErr.stack)
       return res.sendStatus(500)
     }
 
@@ -42,7 +42,7 @@ app.get('/pets/:id', function(req, res){
       return res.sendStatus(404)
     }
 
-    res.set('Content-Type', 'text/plain')
+    res.set('Content-Type', 'application/json')
     res.send(pets[id])
   })
 })
@@ -77,6 +77,87 @@ app.post('/pets', function(req, res) {
       }
       res.set('Content-Type', 'application/json')
       res.send(newPet)
+    })
+  })
+})
+
+//handle a PATCH request to update/overwrite an existing entry
+app.patch('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
+    if (readErr) {
+      console.error(readErr.stack)
+      res.sendStatus(500)
+    }
+    let id = Number.parseInt(req.params.id)
+    let pets = JSON.parse(petsJSON)
+
+    if(id < 0 || id >= pets.length || Number.isNaN(id)) {
+      console.log('non-existent index');
+      return res.sendStatus(404)
+    }
+
+    let age = Number(req.body.age)
+    let kind = req.body.kind
+    let name = req.body.name
+
+    if(!kind && !name && (!age || Number.isNaN(age))) {
+      console.log('incompatible type error');
+      return res.sendStatus(400)
+    }
+
+    if (age) {
+      pets[id]['age'] = age
+    }
+
+    if (name) {
+      pets[id]['name'] = name
+    }
+
+    if (kind) {
+      pets[id]['kind'] = kind
+    }
+
+    let postPet = JSON.stringify(pets[id])
+    let postPets = JSON.stringify(pets)
+
+    fs.writeFile(petsPath, postPets, function(writeErr) {
+      if (writeErr){
+        return res.sendStatus(500)
+      }
+
+      res.set('Content-Type', 'application/json')
+      res.send(postPet)
+    })
+  })
+})
+
+//handle an http DELETE request
+app.delete('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
+    if(readErr) {
+      console.error(readErr.stack)
+      return res.sendStatus(500)
+    }
+    let id = Number.parseInt(req.params.id)
+    let pets = JSON.parse(petsJSON)
+
+    if(id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404)
+    }
+
+    let deletedPet = pets.splice(id, 1)[0]
+    let deletedPetJSON = JSON.stringify(deletedPet)
+
+    let reducedPets = JSON.stringify(pets)
+
+    fs.writeFile(petsPath, reducedPets, function(writeErr) {
+      if (writeErr) {
+        console.error(writeErr.stack)
+        return res.sendStatus(500)
+      }
+
+      res.set('Content-Type', 'application/json')
+      res.send(deletedPet)
     })
   })
 })
